d0ec284dfd153b5d37b4f2badeb8c2a1ac3a4251
Julian Hjortshoj <julian.hjortshoj@emc.com>
2016-04-25 15:06:34 -0700

    Made startdriver script use environment variable for different transports
    Signed-off-by: Mohamed Mohamed <mmohamed@us.ibm.com>

--- acceptance/scripts/startdriver.sh
+++ acceptance/scripts/startdriver.sh
@@ -10,4 +10,12 @@ mkdir -p ../mountdir
 
 driversPath=$(realpath ~/voldriver_plugins)
 #../exec/cephdriver -listenAddr="0.0.0.0:9750" -transport="tcp" -mountDir="../mountdir" -driversPath="${driversPath}" &
+
+
+
+if [$TRANSPORT == "tcp"]
+then
 ../exec/cephdriver -listenAddr="0.0.0.0:9750" -transport="tcp" -driversPath="${driversPath}" &
+else
+../exec/cephdriver -listenAddr="${driversPath}/cephdriver.sock" -transport="unix" -driversPath="${driversPath}" &
+fi
