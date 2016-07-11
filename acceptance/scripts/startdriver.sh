0f8d0ab0c507347cf62ada988acf61e1670e0ca3
Paul Warren <paul.warren@emc.com>
2016-07-11 09:04:50 -0700

    Add driver address to fixture metadata

--- acceptance/scripts/startdriver.sh
+++ acceptance/scripts/startdriver.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
+"${DRIVERS_PATH:?DRIVERS_PATH must be set}"
 
 cd `dirname $0`
 
 pkill -f cephdriver
 
-mkdir -p ~/voldriver_plugins
-
 mkdir -p ../mountdir
-
-driversPath=$(realpath ~/voldriver_plugins)
-#../exec/cephdriver -listenAddr="0.0.0.0:9750" -transport="tcp" -mountDir="../mountdir" -driversPath="${driversPath}" &
-
-
 
 if [ $TRANSPORT == "tcp" ];
 then
 echo "RUNNING TCP ACCEPTANCE"
-../exec/cephdriver -listenAddr="0.0.0.0:9750" -transport="tcp" -driversPath="${driversPath}" &
+../exec/cephdriver -listenAddr="0.0.0.0:9750" -transport="tcp" -driversPath="$DRIVERS_PATH" &
 else
 echo "RUNNING UNIX ACCEPTANCE"
-../exec/cephdriver -listenAddr="${driversPath}/cephdriver.sock" -transport="unix" -driversPath="${driversPath}" &
+../exec/cephdriver -listenAddr="${driversPath}/cephdriver.sock" -transport="unix" -driversPath="$DRIVERS_PATH" &
 fi
